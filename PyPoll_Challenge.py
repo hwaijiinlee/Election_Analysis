65850250103af861b462aa16754a9f5347d64607
hwaijiinlee <90879163+hwaijiinlee@users.noreply.github.com>
2021-10-03 12:32:39 -0500

    Update challenge with deliverable 2

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -101,7 +101,7 @@ with open(file_to_save, "w") as txt_file:
 
          # 6d: Print the county results to the terminal.
         county_results = (
-            f"{county_name}: {cvotes_percentage:.1f}% ({cvotes:,})")
+            f"{county_name}: {cvotes_percentage:.1f}% ({cvotes:,})\n")
         print(county_results)
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
@@ -118,7 +118,7 @@ with open(file_to_save, "w") as txt_file:
     print(county_turnout_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(county_turnout_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
